d5f20b6a05b7f527d2ab27f3f33c58554d4a292c
Matthieu Gavaudan & cool greg <pair+mgavaudan.scoin@devbootcamp.com>
2014-07-03 16:44:05 -0400

    got the view and controller working

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,12 @@
 get '/' do
   # Look in app/views/index.erb
+  @secrets = Secret.all#order(:votes).reverse_order
   erb :index
 end
+
+get '/comments' do
+
+  @comments = Secret.find(params[:secret_id]).comments
+  erb :comments
+end
+
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
-
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $('.secret').click(function(e){
+    e.preventDefault();
+    $.get("/comments", function(data){
+      $('.right').html(data);
+    });
+  });
 });
