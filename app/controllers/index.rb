7b4b9befff26f7cb60d6db2f262a6b9474e7994a
Armen Vartan <vartan.armen@gmail.com>
2014-07-05 01:13:45 -0400

    can access contents in relation to a secret

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,8 @@ get "/" do
   @secrets = Secret.all
   erb :'index'
 end
+
+get "/:sid/comments" do
+  @comments = Comment.where( secret_id: params[:sid] )
+  erb :'comments'
+end
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+  $('.secret').on('click', function(e) {
+    e.preventDefault();
+    .post("/", function(data) {
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+    })
+  })
 });
