585fb62c3e21703b8060d972a53d6840fa193887
Armen Vartan <vartan.armen@gmail.com>
2014-07-05 01:36:30 -0400

    ajax now prints the secret row. need to get it to print the comments associated with the secret

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,9 +1,11 @@
 get "/" do
   @secrets = Secret.all
+  @comments =
   erb :'index'
 end
 
 get "/:sid/comments" do
-  @comments = Comment.where( secret_id: params[:sid] )
+  secret = Secret.find(params[:sid])
+  @comments = secret.comments
   erb :'comments'
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,8 +1,15 @@
 $(document).ready(function() {
   $('.secret').on('click', function(e) {
     e.preventDefault();
-    .post("/", function(data) {
-
+    var secret = $('.secret')
+    console.log(this)
+    $.ajax({
+      url: "/",
+      data: $('this').serialize,
+      success: function(data) {
+        console.log("hi")
+        $("#comments_column").html(data)
+      }
     })
   })
 });
