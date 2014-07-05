91b42f74903303619a82eb88da6041fd5c6050c2
Armen Vartan <vartan.armen@gmail.com>
2014-07-05 00:30:22 -0400

    added a juiciness default to secret

--- db/migrate/20140705002204_create_secrets.rb
+++ db/migrate/20140705002204_create_secrets.rb
@@ -2,8 +2,8 @@ class CreateSecrets < ActiveRecord::Migration
   def change
     create_table :secrets do |t|
       t.string :content
-      t.integer :juiciness
-      
+      t.integer :juiciness, default: 10
+
       t.timestamps
     end
   end
