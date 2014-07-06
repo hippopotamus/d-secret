9c120ee014555396f7c0381b3cd57fb3a29c21e4
Ian Shuff <ianvshuff@gmail.com>
2014-07-06 17:31:59 -0400

    Finished Reloading List

--- public/js/application.js
+++ public/js/application.js
@@ -8,11 +8,13 @@ $(document).ready(function() {
       console.log(response);
       $(".right").html("");
       $(".right").append(response);
+
       $('.right div').hover(function(){
         $(this).animate({"marginLeft":"+=20"}, 100);
       }, function(){
         $(this).animate({"marginLeft":"-=20"}, 100);
       })
+
     })
   })
   $('#submit_secret').click(function(e){
@@ -23,9 +25,13 @@ $(document).ready(function() {
       data: $("#post_secret").serialize(),
       error: function(){
         alert("an error occured");
+      },
+      success: function() {
+        window.location.reload();
       }
     });
   });
+
 
   $('.left a div').hover(function(){
     $(this).animate({"marginLeft":"+=20"}, 100);
