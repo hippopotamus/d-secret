4a38e1fd5b533c52ca6d127787a913ea5c676bf0
Armen Vartan <vartan.armen@gmail.com>
2014-07-06 16:54:10 -0400

    fixed missing }) in js

--- public/js/application.js
+++ public/js/application.js
@@ -1,20 +1,20 @@
 $(document).ready(function() {
   $('a').click(function(e){
     e.preventDefault();
-  $.ajax({
-    url: "/"+$(this).attr("id"),
-    type: "GET"
-  }).done(function(response) {
-    console.log(response);
-    $(".right").html("");
-    $(".right").append(response);
-    $('.right div').hover(function(){
-    $(this).animate({"marginLeft":"+=20"}, 100);
-  }, function(){
-       $(this).animate({"marginLeft":"-=20"}, 100);
-     });
-    });
-
+    $.ajax({
+      url: "/"+$(this).attr("id"),
+      type: "GET"
+    }).done(function(response) {
+      console.log(response);
+      $(".right").html("");
+      $(".right").append(response);
+      $('.right div').hover(function(){
+        $(this).animate({"marginLeft":"+=20"}, 100);
+      }, function(){
+        $(this).animate({"marginLeft":"-=20"}, 100);
+      })
+    })
+  })
   $('#submit_secret').click(function(e){
     e.preventDefault();
     $.ajax({
@@ -30,6 +30,6 @@ $(document).ready(function() {
   $('.left a div').hover(function(){
     $(this).animate({"marginLeft":"+=20"}, 100);
   }, function(){
-     $(this).animate({"marginLeft":"-=20"}, 100);
+    $(this).animate({"marginLeft":"-=20"}, 100);
   });
 });
