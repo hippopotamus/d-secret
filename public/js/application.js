d6b3145ca0ab5e951a035bc03ef0ca0c5c237fa3
Ian Shuff <ianvshuff@gmail.com>
2014-07-06 14:46:30 -0400

    Added a way to Post Secrets but still debugging

--- public/js/application.js
+++ public/js/application.js
@@ -1,8 +1,32 @@
 $(document).ready(function() {
-  $('a').click(function(e){
-    e.preventDefault();
-    $.get(this, function(data){
-      $('.right').html(data);
-    });
+    $('a').click(function(e){
+  	e.preventDefault(); 
+  	$.get(this, function(data){
+  		$('.right').html(data);
+  	});
   });
+
+    $('#submit_secret').click(function(e){
+
+      var postData = $(this).serializeForm();
+
+      e.preventDefault();
+      $.ajax({
+        url: "/new",
+        data: postData,
+        error: function(){
+          alert("an error occured");
+        }
+
+      });
+    });
+
+    
+
+  $('.left a div').hover(function(){
+  	$(this).animate({"marginLeft":"+=20"}, 100);
+  }, function(){
+  	 $(this).animate({"marginLeft":"-=20"}, 100);
+  });
+
 });
