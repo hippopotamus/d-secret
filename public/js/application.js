90b535e1574edb50cd939d30ddf7616271d18142
Matthieu Gavaudan <mgavaudan@hotmail.com>
2014-07-06 19:53:20 -0400

    working on comments

--- public/js/application.js
+++ public/js/application.js
@@ -13,40 +13,37 @@ $(document).ready(function() {
 
     //console.log("/"+$(this).attr("id"));
 
-      $('#submit_comment').click(function(e){
-         e.preventDefault();
-         console.log("hello world!");
-        $.ajax({
-          type: "post",
-          url: "/new_comment",
-          data: $("#post_comment").serialize(),
-          error: function(){
-            console.log("yo");
-            alert("an error occured");
-          },
-          success: function() {
-            console.log("hi");
-             window.location.reload();
-          }
-        });
+    $('#submit_comment').click(function(e){
+       e.preventDefault();
+      $.ajax({
+        type: "post",
+        url: "/new_comment",
+        data: $("#post_comment").serialize(),
+        error: function(){
+          alert("an error occured");
+        },
+        success: function() {
+          window.location.reload();
+        }
       });
-
-
-
-  $('#submit_secret').click(function(e){
-    e.preventDefault();
-    $.ajax({
-      type: "post",
-      url: "/new",
-      data: $("#post_secret").serialize(),
-      error: function(){
-        alert("an error occured");
-      },
-      success: function() {
-        window.location.reload();
-      }
     });
-  });
+
+
+
+    $('#submit_secret').click(function(e){
+      e.preventDefault();
+      $.ajax({
+        type: "post",
+        url: "/new",
+        data: $("#post_secret").serialize(),
+        error: function(){
+          alert("an error occured");
+        },
+        success: function() {
+          window.location.reload();
+        }
+      });
+    });
 
 
 
