b50e383e9f541f4182948283c9c538c22e313eeb
Matthieu Gavaudan <mgavaudan@hotmail.com>
2014-07-06 15:51:41 -0400

    Got Ajax working and css looking fab

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -5,6 +5,6 @@ end
 
 get '/:secret_id' do
   @comments = Secret.find(params[:secret_id]).comments
-  erb :comments
+  erb :comments, :layout => false
 end
 
--- public/js/application.js
+++ public/js/application.js
@@ -1,8 +1,27 @@
 $(document).ready(function() {
   $('a').click(function(e){
     e.preventDefault();
-    $.get(this, function(data){
-      $('.right').html(data);
-    });
+  $.ajax({
+    url: "/"+$(this).attr("id"),
+    type: "GET"
+  }).done(function(response) {
+    console.log(response);
+    $(".right").html("");
+    $(".right").append(response);
+    $('.right div').hover(function(){
+    $(this).animate({"marginLeft":"+=20"}, 100);
+  }, function(){
+     $(this).animate({"marginLeft":"-=20"}, 100);
   });
+  });
+});
+
+  $('.left a div').hover(function(){
+    $(this).animate({"marginLeft":"+=20"}, 100);
+  }, function(){
+     $(this).animate({"marginLeft":"-=20"}, 100);
+  });
+
+
+
 });
