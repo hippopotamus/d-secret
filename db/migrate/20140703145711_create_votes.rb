47d8386b8d5ce279548ab14ea2ec13829c9be0aa
Armen Vartan <vartan.armen@gmail.com>
2014-07-11 13:27:54 -0400

    made og vote bigger

--- db/migrate/20140703145711_create_votes.rb
+++ db/migrate/20140703145711_create_votes.rb
@@ -1,7 +1,7 @@
 class CreateVotes < ActiveRecord::Migration
   def change
       create_table :votes do |t|
-        t.float :number, default: 15
+        t.float :number, default: 20
         t.string :key
         t.belongs_to :secret
 
